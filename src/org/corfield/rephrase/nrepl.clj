10e756cd2999dd18a99efb19c6c11623e2ffb2ae
Sean Corfield <sean@corfield.org>
2026-09-14 09:22:34 -0400

    update jolt nrepl; remove workaround
    Signed-off-by: Sean Corfield <sean@corfield.org>

--- src/org/corfield/rephrase/nrepl.clj
+++ src/org/corfield/rephrase/nrepl.clj
@@ -14,9 +14,6 @@
     (-> msg
         (assoc :nrepl.middleware.caught/caught
                `rephrase/repl-caught)
-        ;; jolt nrepl middleware currently requires this:
-        (assoc :nrepl.middleware.caught/caught-fn
-               rephrase/repl-caught)
         (h))))
 
 (set-descriptor! #'org.corfield.rephrase.nrepl/wrap-rephrase
